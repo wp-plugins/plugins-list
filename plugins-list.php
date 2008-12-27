96f3291f633bce3d7aa592893c224aef5a1ffb56
nutsmuggler <nutsmuggler@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-12-27 14:06:33 +0000

    corrected a couple of typos
    git-svn-id: https://plugins.svn.wordpress.org/plugins-list/tags/0.2.1@82610 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugins-list.php
+++ plugins-list.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Plugins list
 Plugin URI: http://davidebenini.it/wordpress-plugins/plugins-list/
-Description: Displays a list of the active plugins - just insert [plugins list] in posts or pages, betweebìn <ul> and </ul>. If you want to show also the plugins that you have installed but are not presently using, just insert [plugins list all]. ONly one pluin list allowed for post or page.<a href="http://davidebenini.it/credits">Example here</a>.
+Description: Displays a list of the active plugins - just insert [plugins list] in posts or pages, between ul or ol elements. If you want to show also the plugins that you have installed but are not presently using, just insert [plugins list all]. Only one plugin list allowed for post or page. <a href="http://davidebenini.it/credits">Example here</a>.
 Version: 0.2.1
 Author: Davide Benini
 Author URI: http://www.davidebenini.it/
