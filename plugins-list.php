3938f71271bca4fc906d8d505b57c7f2f70f0998
nutsmuggler <nutsmuggler@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-05 19:27:56 +0000

    uploading version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/plugins-list/trunk@99861 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugins-list.php
+++ plugins-list.php
@@ -2,8 +2,9 @@
 /*
 Plugin Name: Plugins list
 Plugin URI: http://davidebenini.it/wordpress-plugins/plugins-list/
-Description: Displays a list of the active plugins - just insert [plugins list] in posts or pages, between ul or ol elements. If you want to show also the plugins that you have installed but are not presently using, just insert [plugins list all]. Only one plugins list allowed for post or page. <a href="http://davidebenini.it/credits">Example here</a>.
-Version: 0.2.2
+Description: Displays a list of the active plugins - just insert [plugins_list] in posts or pages, between ul or ol elements. If you want more custimization, take a look at the <a href='http://davidebenini.it /wordpress-plugins/plugins-list' title='Plugins list documentation'>documentation</a>.  
+
+Version: 1.0
 Author: Davide Benini
 Author URI: http://www.davidebenini.it/
 */
@@ -23,89 +24,60 @@ Author URI: http://www.davidebenini.it/
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-//$my_plugins_version = "0.2.02";
 
-//if (!function_exists("dbenini_register2"))
-  // include (ABSPATH . "wp-content/plugins/my-plugins/dbenini_register_en.php");
 
-if (!function_exists('add_filter'))
-	die ("Hello World!");
+define(DBENINI_DEFAULT_PLUGIN_FORMAT, "<li><a href='#PluginURI#' title='#Title#'>#Title#</a> (v. #Version#) by <a href='#AuthorURI#' title='#Author#']}'>#Author#</a>.</li>");
 
 
 if (!function_exists('get_plugins'))
 				require_once (ABSPATH."wp-admin/includes/plugin.php");
 
-add_filter('the_content', 'dbenini_plugins');
-//dbenini_register2 ("my_plugins", $my_plugins_version);
+add_shortcode('plugins_list', 'plugins_list_func');
+function plugins_list_func($atts) {
+	extract(shortcode_atts(array(
+		'format' => DBENINI_DEFAULT_PLUGIN_FORMAT,
+		'show_inactive' => false,
+	), $atts));
+  $list = dbenini_plugins_list($format, $show_inactive);
+	return $list;
+}
+
+function dbenini_plugins_list($format = DBENINI_DEFAULT_PLUGIN_FORMAT,  $show_inactive = false ) { 
+	$plugins = get_plugins ();
+	$output = "";
+	foreach($plugins as $plugin_file => $plugin_data) {
+		if ($show_inactive || is_plugin_active($plugin_file)) 
+			$output .= dbenini_plugin_with_format($plugin_data, $format);
+	}             
+	
+	return $output;
+}
 
 
+function dbenini_plugin_with_format($plugin_data, $format) { 
+	
+	// Alloweed tag
+	$plugins_allowedtags1 = array('a' => array('href' => array(),'title' => array()),'abbr' => array('title' => array()),'acronym' => array('title' => array()),'code' => array(),'em' => array(),'strong' => array());
+	$plugins_allowedtags2 = array('abbr' => array('title' => array()),'acronym' => array('title' => array()),'code' => array(),'em' => array(),'strong' => array());
 
-
-
-function dbenini_plugins ($content) {
-			global $post, $plugin_css_displayed;
-			$show_inactive_plugins = FALSE;
-			$active_tag = "[plugins list]";
-			$all_tag = "[plugins list all]"; 
-			$bypass_tag = "<!-- Bypass plugins list -->";
-			if ((!strstr($content, $active_tag) && !strstr($content, $all_tag)) || strstr($content,$bypass_tag))
-				return $content;
-        	if (strstr($content, $active_tag)) { 
-				$actual_tag = $active_tag;
-			} else { 
-				$actual_tag = $all_tag;
-				$show_inactive_plugins = TRUE;
-				}
-			$plugins = get_plugins ();				
-			
-			
-			if (empty($plugins)) {
-				echo '<p>';
-				_e("Couldn&#8217;t open plugins directory or there are no plugins available."); // TODO: make more helpful
-				echo '</p>';
-			} else {
-			ob_start();
-			$i = 1;
-			?>
-			
-			<?php
-				$style = '';
-			    
-				foreach($plugins as $plugin_file => $plugin_data) {
-			
-	  
-			                          
-			
-					$plugins_allowedtags1 = array('a' => array('href' => array(),'title' => array()),'abbr' => array('title' => array()),'acronym' => array('title' => array()),'code' => array(),'em' => array(),'strong' => array());
-					$plugins_allowedtags2 = array('abbr' => array('title' => array()),'acronym' => array('title' => array()),'code' => array(),'em' => array(),'strong' => array());
-			
-					// Sanitize all displayed data
-					$plugin_data['Title']       = wp_kses($plugin_data['Title'], $plugins_allowedtags1);
-					$plugin_data['PluginURI']       = wp_kses($plugin_data['PluginURI'], $plugins_allowedtags1);
-					$plugin_data['AuthorURI']       = wp_kses($plugin_data['AuthorURI'], $plugins_allowedtags1); 
-					$plugin_data['Version']     = wp_kses($plugin_data['Version'], $plugins_allowedtags1);
-					// $plugin_data['Description'] = wp_kses($plugin_data['Description'], $plugins_allowedtags2);
-					$plugin_data['Author']      = wp_kses($plugin_data['Author'], $plugins_allowedtags1);
-					//$plugin_data['Description'] = preg_replace ("@<a .*?".">(.*?)</a>@mis", "\\1", $plugin_data['Description']); 
-			
-				   
-			    				
-					
-			
-					if ($show_inactive_plugins | is_plugin_active($plugin_file)) {
-					   echo "<li><a href='{$plugin_data['PluginURI']}' title='{$plugin_data['Title']}'>{$plugin_data['Title']}</a> (v. {$plugin_data['Version']} ) by <a href='{$plugin_data['AuthorURI']}' title='{$plugin_data['Author']}'>{$plugin_data['Author']}</a>.</li>";
-					}
-				 //do_action( 'after_plugin_row', $plugin_file );
-				 $i++;
-				}
-				
-			?>
-		
-		    
-<?php 			
-        $ret = ob_get_contents();
-        ob_end_clean ();
-        return str_replace ($actual_tag, $ret, $content);
-     }
+	// Sanitize all displayed data
+	$plugin_data['Title']       = wp_kses($plugin_data['Title'], $plugins_allowedtags1);
+	$plugin_data['PluginURI']       = wp_kses($plugin_data['PluginURI'], $plugins_allowedtags1);
+	$plugin_data['AuthorURI']       = wp_kses($plugin_data['AuthorURI'], $plugins_allowedtags1); 
+	$plugin_data['Version']     = wp_kses($plugin_data['Version'], $plugins_allowedtags1);
+	//$plugin_data['Description'] = wp_kses($plugin_data['Description'], $plugins_allowedtags2);
+	$plugin_data['Author']      = wp_kses($plugin_data['Author'], $plugins_allowedtags1);
+	//$plugin_data['Description'] = preg_replace ("@<a .*?".">(.*?)</a>@mis", "\\1", $plugin_data['Description']);
+	
+	
+	$format = str_replace("#Title#", $plugin_data['Title'], $format);     
+	$format = str_replace("#PluginURI#", $plugin_data['PluginURI'], $format); 
+	$format = str_replace("#AuthorURI#", $plugin_data['AuthorURI'], $format);     
+	$format = str_replace("#Version#", $plugin_data['Version'], $format);     
+	$format = str_replace("#Description#", $plugin_data['Description'], $format);     
+	$format = str_replace("#Author#", $plugin_data['Author'], $format);     
+	$format = str_replace("#LinkedTitle#", "<a href='".$plugin_data['PluginURI']."' title='".$plugin_data['Title']."'>".$plugin_data['Title']."</a>", $format);
+  $format = str_replace("#LinkedAuthor#", "<a href='".$plugin_data['AuthorURI']."' title='".$plugin_data['Author']."'>".$plugin_data['Author']."</a>", $format);
+	return $format;
 }
 ?>
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://davidebenini.it/wordpress-plugins
 Tags: comments
 Requires at least: 2.5.1
 Tested up to: 2.7
-Stable tag: /tags/0.2.2/
+Stable tag: /tags/1.0/
 
 This plugins allows you to insert a list of the Wordpress plugins you are using into any post/page.
 
@@ -14,7 +14,9 @@ This is a simple Wordpress plug-in aimed at giving credit where credit is due.
 
 I am talking about those wonderful plug-in creators who make our life easier, allowing us to do wonderful things without any knowledge of php. After completing my website I felt I had to thanks those people and quote them somewhere. This plug-in lists all the plugins you are using. The plugin can inserts a simple html list into any post/page.
 
-*Plugins list* is directly derived from [My plugins](http://wordpress.org/extend/plugins/my-plugins/), a plug-in by [Matej Nastran](http://matej.nastran.net/) which produces a table of installed plug-ins. I made this plug-in because I was after a simple list, and I am not planning to go much further; if you are looking for something more full-featured, try [My plugins](http://wordpress.org/extend/plugins/my-plugins/).
+Plugins List allows to insert a simple list of your plugins into you posts and pages through a shortcode. If you're into customisation, you can specify a format argument and indicate the exact data you are after. There's also an option to display also inactive plugins.
+
+My thanks to [Matej Nastran](http://matej.nastran.net/)'s [My plugins](http://wordpress.org/extend/plugins/my-plugins/), from which *Plugins list* was initially derived.
 
 == Installation ==
 
@@ -23,21 +25,28 @@ To get a list of the plugins that are installed and activated in your website, i
 	          
 	
     <ul>
-    [plugins list]
+    [plugins_list]
 	 </ul>
 
+You can customise the output specifying the `format` argument. Here's an example:
 
+	<table>
+		<tr>
+			<th>Title</th><th>Author</th>
+		</tr>
+	    [plugins_list format="<tr><td>#LinkedTitle#</td><td>#LinkedAuthor#</td></tr>"]
+	</table>
+
+The placeholders are: `#Title#`, `#PluginURI#`, `#Author#"` ,`#AuthorURI#`, `#Version#`, `#Description#`, `#LinkedTitle#`, `#LinkedAuthor#`. I guess their meaning is self evident.
+ 
 If, for some obscure reason (I cannot think of any), you want to list also the plug-ins you have installed but are not using, here's the formula:
 
 
     <ul>
-    [plugins list all]
+    [plugins_list show_inactive='true']
     </ul>
 
-
-Only a single plug-ins list is allowed per post/page. (At this stage at least.)
-
-The plug-in list can be freely styled with css, just place any *class* or *id* attribute on the *ul* element.       
+The plugins list can be freely styled with css, just place any *class* or *id* attribute on the `format` string, or on the elements surrounding it.     
 
 == Frequently Asked Questions ==
 
