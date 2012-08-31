2fd0463f6c2bde4186e000f0e068702ba49b7a63
dartiss <dartiss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-31 15:03:34 +0000

    git-svn-id: https://plugins.svn.wordpress.org/plugins-list/tags/2.1@592942 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugins-list.php
@@ -0,0 +1,41 @@
+<?php
+/*
+Plugin Name: Artiss Plugins List
+Plugin URI: http://www.artiss.co.uk/plugins-list
+Description: Displays a list of the active plugins
+Version: 2.1
+Author: David Artiss
+Author URI: http://www.artiss.co.uk
+*/
+
+/**
+* Artiss Plugins List
+*
+* Main code - include various functions
+*
+* @package	Artiss-Plugins-List
+* @since	2.1
+*/
+
+define( 'plugins_list_version', '2.1' );
+
+define( APL_DEFAULT_PLUGIN_FORMAT, "<li><a href='#PluginURI#' title='#Title#'>#Title#</a> by <a href='#AuthorURI#' title='#Author#']}'>#Author#</a>.</li>" );
+
+if ( !function_exists( 'get_plugins' ) ) { require_once ( ABSPATH . 'wp-admin/includes/plugin.php' ); }
+
+$functions_dir = WP_PLUGIN_DIR . '/plugins-list/includes/';
+
+// Include all the various functions
+
+if ( is_admin() ) {
+
+    include_once( $functions_dir . 'apl-admin-config.php' );		        // Assorted admin configuration changes
+
+} else {
+
+	include_once( $functions_dir . 'apl-shortcodes.php' );       		    // Define shortcodes
+
+	include_once( $functions_dir . 'apl-generate-list.php' );       		// Generate the list of plugins
+
+}
+?>
